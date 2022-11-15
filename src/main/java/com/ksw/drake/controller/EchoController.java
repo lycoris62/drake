404a9ff24dd06437fb9978c58cc6e53d29cf9c6c
Lycoris62 <wodbs7758@naver.com>
2022-11-16 03:15:23 +0900

    change vo to JSONObject and post-mapping code. & delete vo class.

--- src/main/java/com/ksw/drake/controller/EchoController.java
+++ src/main/java/com/ksw/drake/controller/EchoController.java
@@ -1,44 +1,35 @@
 package com.ksw.drake.controller;
 
-import com.ksw.drake.vo.*;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 public class EchoController {
 
-//    vo 디렉토리랑 이 매핑은 밑의 jsonobject로 대체할 예정입니다 그에 따라 vo들 다 사라질 예정이니 놀라지 마세요
     @PostMapping("/api/echo")
-    public ResponseMessageVO message(@RequestBody RequestMessageVO vo) {
-        ResponseMessageVO resVO = new ResponseMessageVO();
-        UserRequestVO userRequest = vo.getUserRequest();
-        String utterance = userRequest.getUtterance();
-        DataVO data = new DataVO();
+    public JSONObject message(@RequestBody String req) throws ParseException {
+        JSONParser parser = new JSONParser();
+        JSONObject reqJSON = (JSONObject) parser.parse(req);
+        JSONObject UserRequest = (JSONObject) reqJSON.get("userRequest");
 
-        String str = "echo: " + utterance;
+        JSONObject jsonObject = new JSONObject();
+        JSONArray outputsArray = new JSONArray();
+        JSONObject outputsObject = new JSONObject();
+        JSONObject output = new JSONObject();
+        JSONObject text = new JSONObject();
 
-        data.setMsg(str);
-        resVO.setData(data);
+        jsonObject.put("version", "2.0");
+        text.put("text", "echo: " + UserRequest.get("utterance"));
+        output.put("simpleText", text);
+        outputsArray.add(output);
+        outputsObject.put("outputs", outputsArray);
 
-        SimpleTextVO simpleText = new SimpleTextVO();
-        simpleText.setText(str);
+        jsonObject.put("template", outputsObject);
 
-        OutputsVO outputs = new OutputsVO();
-        outputs.setSimpleText(simpleText);
-        List<OutputsVO> outputsList = new ArrayList<>();
-        outputsList.add(outputs);
-
-        TemplateVO template = new TemplateVO();
-        template.setOutputs(outputsList);
-
-        resVO.setVersion("2.0");
-        resVO.setTemplate(template);
-
-        return resVO;
+        return jsonObject;
     }
 
 //    쉬운 JSON 예제를 만들어뒀습니다. 이거보며 연습해보세요
