b572629974881ba12dd21c38b31dfbfe795a9ccf
Lycoris62 <wodbs7758@naver.com>
2022-11-17 10:46:25 +0900

    edit create schedule db query. 7

--- src/main/java/com/ksw/drake/repository/JDBCScheduleRepository.java
+++ src/main/java/com/ksw/drake/repository/JDBCScheduleRepository.java
@@ -8,8 +8,15 @@ import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.sql.DataSource;
 import java.sql.ResultSet;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
+
+
+//DateUtil.java
+
+
 
 public class JDBCScheduleRepository implements ScheduleRepository{
 
@@ -30,7 +37,20 @@ public class JDBCScheduleRepository implements ScheduleRepository{
         String date = (String) params.get("sys_date_time");
         JSONObject dateJSON = (JSONObject) parser.parse(date);
         System.out.println(dateJSON);
-        Date nowDate = (Date) dateJSON.get("date");
+
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String nowDate = (String) dateJSON.get("date");
+
+
+        Date targetDate = getDate(
+                Integer.parseInt(nowDate.split("-")[0]),
+                Integer.parseInt(nowDate.split("-")[1]),
+                Integer.parseInt(nowDate.split("-")[2]));
+
+//        Date targetDate = dateFormat.format(nowDate);
+
+//        System.out.println("Date: "+dateFormat.format(nowDate));
+
         JSONObject user = (JSONObject) userRequest.get("user");
         String member_id = (String) user.get("id");
         String memberIdQuery = "Select member_id from \"Member\" where member_id = \'" + member_id + "\'";
@@ -43,11 +63,11 @@ public class JDBCScheduleRepository implements ScheduleRepository{
 //        String scheduleName = "name";
 //        Date nowDate = new Date();
         String query2 = "INSERT INTO public.\"Schedule\"(schedule_name, target_date, member_id) VALUES (?, ?, ?)";
-        jdbcTemplate.update(query2, scheduleName, nowDate, member_id);
+        jdbcTemplate.update(query2, scheduleName, targetDate, member_id);
 
         JSONObject jsonObject2 = new JSONObject();
         jsonObject2.put("scheduleName", scheduleName);
-        jsonObject2.put("targetDate", nowDate);
+        jsonObject2.put("targetDate", targetDate);
         jsonObject2.put("memberId", member_id);
 
         return jsonObject2;
@@ -61,5 +81,11 @@ public class JDBCScheduleRepository implements ScheduleRepository{
     @Override
     public List<ScheduleDTO> findAll() {
         return null;
+    }
+
+    public static Date getDate(int year, int month, int date) {
+        Calendar cal = Calendar.getInstance();
+        cal.set(year, month - 1, date);
+        return new Date(cal.getTimeInMillis());
     }
 }
