69c2875bb1a8a439621723c0b8301c0e67a2e8f4
Lycoris62 <wodbs7758@naver.com>
2022-11-22 21:03:02 +0900

    add @RestContoller in MemoController.

--- src/main/java/com/ksw/drake/controller/MemoController.java
+++ src/main/java/com/ksw/drake/controller/MemoController.java
@@ -4,9 +4,12 @@ import com.ksw.drake.service.MemoService;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.ParseException;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class MemoController {
     private MemoService memoService;
 
